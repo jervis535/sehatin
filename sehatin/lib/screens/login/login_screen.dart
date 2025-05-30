29d266ee9b6c35e079b7e2a0dc181c9743ef6bce
fab535230016 <fablius.535230016@stu.untar.ac.id>
2025-05-30 10:57:47 +0700

    Styling For Login and Register

--- sehatin/lib/screens/login/login_screen.dart
+++ sehatin/lib/screens/login/login_screen.dart
@@ -3,13 +3,49 @@ import 'login_form.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: const Padding(
-        padding: EdgeInsets.all(16),
-        child: LoginForm(),
+      backgroundColor: const Color(0xFFFCEEEE),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                // Custom Wave Background
+                Container(
+                  height: 220,
+                  width: double.infinity,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Image.asset(
+                      'assets/wave.png', // export bentuk wave dari Figma
+                      fit: BoxFit.cover,
+                      height: 220,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 120,
+                  left: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.person, size: 80, color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 90),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: LoginForm(),
+            ),
+          ],
+        ),
       ),
     );
   }
